3f5efbc01389f2a64f3f7a9ad72656489980d9c2
WEB-PC6\User <vakhromeevae@gmail.com>
2018-02-08 09:33:31 +0300

    More convinient functions.

--- MaskedInput.js
+++ MaskedInput.js
@@ -3,29 +3,64 @@ $(function(){
     $("#phone").mask("8(999) 999-9999");
 
     //Email validation
-    errorMail = false;
+    var errors = false;
+
     $("#email").focusout(function(){
         var value = $(this).val().trim();
         if (!(isEmail(value)) && value.length > 0) {
             $(this).notify("Некорректный email", "error");
             $(this).addClass("errtextbox");
-            errorMail = true;
+            errors = true;
         } else {
             $(this).removeClass("errtextbox");
-            errorMail = false;
+            errors = false;
+        }
+    });
+
+    $('#square').on('input', function(){
+        var re = /^\d+\.?(\d{1,2})?$/ig,
+            cVal = $.trim($(this).val());
+        $(this).val(cVal.replace(/,/g, '.'));
+        if(!(re.test(cVal.replace(",",".")))|| cVal.length > 6){
+            $(this).val(cVal.substr(0,cVal.length-1));
+        }
+    });
+
+
+    $(".required").focusout(function(){
+        var value = $(this).val().trim();
+        if(value.length < 1){
+            $(this).notify("Поле должно быть заполнено.", "error");
+        }
+    });
+
+    $('#price').on('input', function(){
+        var re = /^\d+\.?(\d{1,2})?$/ig,
+            cVal = $.trim($(this).val());
+        $(this).val(cVal.replace(/,/g, '.'));
+        if(!(re.test(cVal.replace(",",".")))|| cVal.length > 10){
+            $(this).val(cVal.substr(0,cVal.length-1));
         }
     });
 
     $("#send").click(function(){
-        if (!(errorMail)) {
+        if (!(isError())) {
             $("#regForm").submit();
         } else {
-            $(this).notify("Форма пустая или заполнена некорректно", "error");
+            $(this).notify("Не все обязательные поля верно заполнены.", "error");
         }
     });
 
     function isEmail(email) {
         var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         return regex.test(email);
+    }
+
+    function isError() {
+        if (('#square').val().empty() || ('#price').val().empty()  ||
+            ('#phone').val().empty()  ||  errors == true
+        ){
+            return true;
+        } else return false;
     }
 });
